4822fa12b07eb825bb189d9c74777da4bc1542d4
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-08 14:55:49 +0600

    created js object and given min 3 style on h2 in article tag

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
+const styleObj = { fontSize: '2rem', color: 'red', backgroundColor: 'lightgreen' };
+
 function App() {
   return (
     <div className="App">
       <article className='blog'>
-        <h2>My Blog</h2>
-        <p>
+        <h2 style={styleObj}>My Blog</h2>
+        <p style={{ color: 'red' }}>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quisquam dolor eveniet? Pariatur incidunt voluptate a, molestiae sapiente culpa cupiditate eius accusantium corrupti reiciendis ullam velit eveniet necessitatibus fuga, nobis dolor quae in unde quam eligendi voluptatum! Dignissimos reprehenderit inventore totam culpa in nam voluptatibus, ducimus sit? Soluta, amet ullam.
         </p>
       </article>
