b82ac45d0b0e78f4fc470f475bc00330975d039c
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-08 15:06:07 +0600

    created blog component and also created another 3 components

--- src/App.js
+++ src/App.js
@@ -11,7 +11,34 @@ function App() {
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quisquam dolor eveniet? Pariatur incidunt voluptate a, molestiae sapiente culpa cupiditate eius accusantium corrupti reiciendis ullam velit eveniet necessitatibus fuga, nobis dolor quae in unde quam eligendi voluptatum! Dignissimos reprehenderit inventore totam culpa in nam voluptatibus, ducimus sit? Soluta, amet ullam.
         </p>
       </article>
+      <Blog></Blog>
     </div>
+  );
+}
+
+const Blog = () => {
+  return (
+    <div>
+      <h3>Parent Blog</h3>
+    </div>
+  );
+}
+
+const BlogOne = (props) => {
+  return (
+    <div></div>
+  );
+}
+
+const BlogTwo = (props) => {
+  return (
+    <div></div>
+  );
+}
+
+const BlogThree = (props) => {
+  return (
+    <div></div>
   );
 }
 
